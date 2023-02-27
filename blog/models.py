accd45e218647d5cbe1f0519fc066204ff197764
Mahdi <mmf1344py@gmail.com>
2023-02-28 00:39:11 +0330

    add datetime for publish-create and update

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 
 class Post(models.Model):
+    
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
+    
+    publish = models.DateTimeField(default=timezone.now)
+    
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
