9c3169095a7be7a839653139e98bc278b0219ba8
Mahdi <mmf1344py@gmail.com>
2023-02-28 00:55:01 +0330

    defining a default sort order

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 
 
 class Post(models.Model):
-    
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
@@ -12,6 +12,9 @@ class Post(models.Model):
     
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["-publish"]
 
     def __str__(self):
         return self.title
