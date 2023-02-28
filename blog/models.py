ab30b115178bc7425edd2c7e6b16bdd2ebb93401
Mahdi <mmf1344py@gmail.com>
2023-02-28 10:10:07 +0330

    adding status field and Status class with TextChoices type

--- blog/models.py
+++ blog/models.py
@@ -4,20 +4,30 @@ from django.utils import timezone
 
 class Post(models.Model):
 
+    class Status(models.TextChoices):
+        DRAFT = "DF", "Draft"
+        PUBLISHED = "PB", "Published"
+
+
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     body = models.TextField()
     
     publish = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=2,
+                             choices=Status.choices,
+                             default=Status.DRAFT)
     
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
+
 
     class Meta:
         ordering = ["-publish"]
         indexes = [
             models.Index(fields=["-publish"])
         ]
+
 
     def __str__(self):
         return self.title
