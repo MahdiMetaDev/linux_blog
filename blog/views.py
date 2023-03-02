fda09726cf3b0cdf980ce8a9133c9133dc8d4da3
Mahdi <mmf1344py@gmail.com>
2023-03-02 14:58:00 +0330

    edit some configurations in list and detail views

--- blog/views.py
+++ blog/views.py
@@ -8,7 +8,7 @@ def posts_list_view(request):
     posts = Post.published.all()
 
     return render(request,
-                 "blog/post/list.html",
+                 "blog/posts_list.html",
                  {"posts": posts})
 
 
@@ -19,5 +19,5 @@ def post_detail_view(request, id):
         raise Http404("No Post found.")
     
     return render(request,
-                 "blog/post/detail.html",
+                 "blog/post_detail.html",
                  {"post": post})
