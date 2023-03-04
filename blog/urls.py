1d44873ce1306ae99572ef11fd1818807e9b7adb
Mahdi <mmf1344py@gmail.com>
2023-03-04 19:35:12 +0330

    replace posts_list_view with PostListView as a class-based-view

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name = "blog"
 
 urlpatterns = [
-    path("", views.posts_list_view, name="posts_list"),
+    # path("", views.posts_list_view, name="posts_list"),
+    path("", views.PostListView.as_view(), name="posts_list"),
     path("<int:year>/<int:month>/<int:day>/<slug:post>/",
         views.post_detail_view,
         name="post_detail"),
--- blog/views.py
+++ blog/views.py
@@ -1,29 +1,40 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+# from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views import generic
 
 from .models import Post
 
 
-def posts_list_view(request):
+# def posts_list_view(request):
     
-    posts_list = Post.published.all()
+#     posts_list = Post.published.all()
 
-    # Pagination with 3 posts per page
-    paginator = Paginator(posts_list, 3)
-    page_number = request.GET.get("page", 1)
+#     # Pagination with 3 posts per page
+#     paginator = Paginator(posts_list, 3)
+#     page_number = request.GET.get("page", 1)
 
-    try:
-        posts = paginator.page(page_number)
-    except PageNotAnInteger:
-        # If page_number is not an integer deliver the first page
-        posts = paginator.page(1)
-    except EmptyPage:
-        # If page_number is out of range deliver last page of results
-        posts = paginator.page(paginator.num_pages)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         # If page_number is not an integer deliver the first page
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         # If page_number is out of range deliver last page of results
+#         posts = paginator.page(paginator.num_pages)
     
-    return render(request,
-                 "blog/posts_list.html",
-                 {"posts": posts})
+#     return render(request,
+#                  "blog/posts_list.html",
+#                  {"posts": posts})
+
+
+class PostListView(generic.ListView):
+    """
+    Alternative post list view
+    """
+    queryset = Post.published.all()
+    context_object_name = "posts"
+    paginate_by = 3
+    template_name = "blog/posts_list.html"
 
 
 def post_detail_view(request, year, month, day, post):
