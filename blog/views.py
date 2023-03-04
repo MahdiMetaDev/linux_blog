c59fac49f1d027f2429c961fe9e43ba9ee79b928
Mahdi <mmf1344py@gmail.com>
2023-03-04 19:18:01 +0330

    Exception Handling for pagination, create pagination html template

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Post
 
@@ -8,9 +8,18 @@ def posts_list_view(request):
     
     posts_list = Post.published.all()
 
+    # Pagination with 3 posts per page
     paginator = Paginator(posts_list, 3)
     page_number = request.GET.get("page", 1)
-    posts = paginator.page(page_number)
+
+    try:
+        posts = paginator.page(page_number)
+    except PageNotAnInteger:
+        # If page_number is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page_number is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
     
     return render(request,
                  "blog/posts_list.html",
